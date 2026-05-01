da94097dfc6fad1f030e0be1f46dc170ebc58a87
SAGArSHARMa1745 <sagarrrsharma54@gmail.com>
2026-05-01 18:41:06 +0530

    Update db.py

--- language_bot/modules/db.py
+++ language_bot/modules/db.py
@@ -4,19 +4,42 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1234",     
+        password="1234",
         database="test_db"
     )
 
-def save_result(word, correct):
+
+def save_bronze(word, raw_json):
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute(
-        "INSERT INTO results (word, correct) VALUES (%s, %s)",
-        (word, correct)
+        "INSERT INTO bronze (word, raw_json) VALUES (%s, %s)",
+        (word, raw_json)
     )
+    conn.commit()
+    cur.close()
+    conn.close()
 
+
+def save_silver(word, meaning, example):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO silver (word, meaning, example) VALUES (%s, %s, %s)",
+        (word, meaning, example)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def save_gold(word, correct, priority):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO gold (word, correct, priority) VALUES (%s, %s, %s)",
+        (word, correct, priority)
+    )
     conn.commit()
     cur.close()
     conn.close()
@@ -25,7 +48,7 @@ def get_stats():
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT correct FROM results")
+    cur.execute("SELECT correct FROM gold")
     data = cur.fetchall()
 
     cur.close()
